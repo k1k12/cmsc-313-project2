bc49c231581dbddec166a8755a033f1adf16ffb4
kiki <sofia@k1k12.localdomain>
2026-04-27 18:54:56 -0400

    feat: program outputs input. in like 6000 lines

--- src/double.s
+++ src/double.s
@@ -1,9 +1,17 @@
-# fooooooooooo
+# Assembly program that asks user for a number then prints out double the value
 
 .section .data
 prompt:
   .ascii "Enter no -> "
 prompt_length = . - prompt
+
+output:
+  .ascii "number is -> "
+output_length = . - output
+
+.section .bss
+input:
+  .skip 32
 
 .section .text
 .globl _start
@@ -14,6 +22,30 @@ _start:
   movq $1, %rdi # 1 = stdout
   movq $prompt, %rsi # addr msg
   movq $prompt_length, %rdx # length odf mgg
+  syscall
+
+  # read 0 input prompt_length
+  movq $0, %rax # syscall num for read
+  movq $0, %rdi # 0 = stdin
+  movq $input, %rsi # buffer
+  movq $32, %rdx # max bytes to read
+  syscall
+
+  # save no bytes read
+  movq %rax, %r8
+
+  # write 1 output output_length
+  mov $1, %rax # syscall number for write
+  movq $1, %rdi # 1 = stdout
+  movq $output, %rsi # addr msg
+  movq $output_length, %rdx # length odf mgg
+  syscall
+
+  # write 1 prompt prompt_length
+  mov $1, %rax # syscall number for write
+  movq $1, %rdi # 1 = stdout
+  movq $input, %rsi # addr msg
+  movq %r8, %rdx # length odf mgg
   syscall
 
   # exit
