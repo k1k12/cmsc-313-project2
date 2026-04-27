bf51a1c81f9aed9425dc90e6596ca370a0500bea
kiki <sofia@k1k12.localdomain>
2026-04-27 18:46:47 -0400

    feat: print enter no prompt

--- src/double.s
+++ src/double.s
@@ -1,11 +1,22 @@
-// fooooooooooo
+# fooooooooooo
 
-.global _start
+.section .data
+prompt:
+  .ascii "Enter no -> "
+prompt_length = . - prompt
 
 .section .text
+.globl _start
 
 _start:
-  // just exit
-  mov $60, %rax
-  xor %rdi, %rdi
+  # write 1 prompt prompt_length
+  mov $1, %rax # syscall number for write
+  movq $1, %rdi # 1 = stdout
+  movq $prompt, %rsi # addr msg
+  movq $prompt_length, %rdx # length odf mgg
+  syscall
+
+  # exit
+  mov $60, %rax # this is syscall no for exit
+  xor %rdi, %rdi # exit status 0
   syscall
